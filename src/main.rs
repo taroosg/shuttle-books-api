9b2df5747bdc48b94324e0af286d2b409e95510f
taroosg <root@taroosg.dev>
2024-11-29 14:01:55 +0900

    書籍管理機能を追加

--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,43 @@ use axum::{
 use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, PgPool};
 
+async fn list(State(state): State<MyState>) -> Result<impl IntoResponse, impl IntoResponse> {
+    match sqlx::query_as::<_, Book>("SELECT * FROM books")
+        .fetch_all(&state.pool)
+        .await
+    {
+        Ok(books) => Ok((StatusCode::OK, Json(books))),
+        Err(e) => Err((StatusCode::BAD_REQUEST, e.to_string())),
+    }
+}
+
 async fn retrieve(
     Path(id): Path<i32>,
     State(state): State<MyState>,
 ) -> Result<impl IntoResponse, impl IntoResponse> {
-    match sqlx::query_as::<_, Todo>("SELECT * FROM todos WHERE id = $1")
+    match sqlx::query_as::<_, Book>("SELECT * FROM books WHERE id = $1")
         .bind(id)
         .fetch_one(&state.pool)
         .await
     {
-        Ok(todo) => Ok((StatusCode::OK, Json(todo))),
+        Ok(book) => Ok((StatusCode::OK, Json(book))),
         Err(e) => Err((StatusCode::BAD_REQUEST, e.to_string())),
     }
 }
 
 async fn add(
     State(state): State<MyState>,
-    Json(data): Json<TodoNew>,
+    Json(data): Json<BookNew>,
 ) -> Result<impl IntoResponse, impl IntoResponse> {
-    match sqlx::query_as::<_, Todo>("INSERT INTO todos (note) VALUES ($1) RETURNING id, note")
-        .bind(&data.note)
-        .fetch_one(&state.pool)
-        .await
+    match sqlx::query_as::<_, Book>(
+        "INSERT INTO books (title, isbn) VALUES ($1, $2) RETURNING id, title, isbn",
+    )
+    .bind(&data.title)
+    .bind(&data.isbn)
+    .fetch_one(&state.pool)
+    .await
     {
-        Ok(todo) => Ok((StatusCode::CREATED, Json(todo))),
+        Ok(book) => Ok((StatusCode::CREATED, Json(book))),
         Err(e) => Err((StatusCode::BAD_REQUEST, e.to_string())),
     }
 }
@@ -50,20 +63,23 @@ async fn main(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::Shut
 
     let state = MyState { pool };
     let router = Router::new()
-        .route("/todos", post(add))
-        .route("/todos/:id", get(retrieve))
+        .route("/books", get(list))
+        .route("/books", post(add))
+        .route("/books/:id", get(retrieve))
         .with_state(state);
 
     Ok(router.into())
 }
 
 #[derive(Deserialize)]
-struct TodoNew {
-    pub note: String,
+struct BookNew {
+    pub title: String,
+    pub isbn: String,
 }
 
 #[derive(Serialize, FromRow)]
-struct Todo {
+struct Book {
     pub id: i32,
-    pub note: String,
+    pub title: String,
+    pub isbn: String,
 }
